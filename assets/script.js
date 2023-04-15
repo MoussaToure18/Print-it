f417ea59e61e35818d4224733e0b40b8abe5e839
Moussa Toure <aboualyasandy@gmail.com>
2023-04-15 17:12:21 +0200

    commit

--- assets/script.js
+++ assets/script.js
@@ -22,12 +22,67 @@ const slides = [
 
 
 let arrow_left = document.querySelector('.arrow_left')
-let arrow_right = document.querySelector('.arrow_left')
+let arrow_right = document.querySelector('.arrow_right')
+
+let img = document.querySelector('.banner-img')
+let courantslides = 0
+
 
 arrow_left.addEventListener('click', function () {
-	alert('fleche gauche')
+	/*alert('fleche gauche')*/
+	courantslides = courantslides - 1 //courantslide-- 
+	img.src = './assets/images/slideshow/' + slides[courantslides].image
+
 })
 
 arrow_right.addEventListener('click', function () {
-	alert('fleche droite')
-})
+	/*alert('fleche droite')*/
+	courantslides = courantslides + 1 //courantslide++ 
+	img.src = './assets/images/slideshow/' + slides[courantslides].image
+});
+
+
+
+const items = document.querySelectorAll('img');
+const nbSlide = items.length;
+const suivant = document.querySelector('arrow_right');
+const precedent = document.querySelector('arrow_left');
+let count = 0;
+
+//function slidesuivante() {
+//items[count].classList.remove('banner-img')
+
+//if (count < nbSlide - 1) {
+//	count++
+//} else {
+//	count = 0
+//}
+
+//items[count].classList.add('banner-img')
+//console.log(count);
+//}
+
+
+
+let dots = ""
+for (let i = 0; i < slides.length; i++) {
+
+
+	if (courantslides == i) {
+		dots += "<span class='dot dot_selected'></span>"
+	}
+	else {
+		dots += "<span class='dot'></span>"
+	}
+}
+
+let slidesHTML = document.querySelector('.dots')
+slidesHTML.innerHTML = dots
+
+/*let img = 1
+
+let imgHTML = document.querySelector('banner-img')
+imgHTML.src = slider[img].image
+
+let textHTML = document.querySelector('benner-text')
+textHTML.innerHTML = slider.text*/
